02ff92b1f9a6470b35713daa8c0ea2a4b1cc3537
Lindokuhle Shweni <127991096+Shweni@users.noreply.github.com>
2023-05-17 22:54:15 +0200

    Add files via upload

---
+++ Challenge1.js
@@ -0,0 +1,19 @@
+const leoName = 'Leo Musvaire'
+const leoNumber = '2'
+const leoStreet = 'Church St.'
+const leoPostal = '3105'
+const leoBalance = '-10'
+
+const sarahName = 'Sarah    '
+const sarahSurname = 'Kleinhans'
+const sarahBalance = '-4582.21000111'
+const sarahNumber = '13'
+const sarahStreet = 'William Close'
+const sarahPostal = '0310'
+
+// Only change below this line
+
+
+
+console.log(leoName , leoStreet,leoNumber, leoPostal)
+console.log(sarahName, sarahStreet, sarahNumber, sarahPostal)
